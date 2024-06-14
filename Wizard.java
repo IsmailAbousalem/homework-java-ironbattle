eebebcc059da9f021a85b0207ce19ab50aaf0247
NomadRiding <fundora.a@gmail.com>
2024-06-13 21:25:57 -0400

    added constructor

--- Wizard.java
+++ Wizard.java
@@ -1,6 +1,36 @@
-public class Wizard extends Character{
-    private int mana;
-    private int intelligence;
+import java.util.Random;
+
+public class Wizard extends Character implements Attacker{
+    private int mana = new Random().nextInt(50)+ 1;
+    private int intelligence new Random().nextInt(50)+ 1;
 
 
+    public Wizard(String name, int hp, int mana, boolean isAlive, int intelligence){
+        super(name, hp, isAlive);
+        this.mana = mana;
+        this.intelligence = intelligence;
+    }
+
+    //Getter for mana
+    public int getMana() {
+        return mana;
+    }
+
+    //Setter for mana
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    //getter for intel
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    //setter for intel
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    //Override goes here to implement attack()
 }
+
